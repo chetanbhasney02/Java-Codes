7348520c32bce4ce50dc9dfe5d7e5f1ebc2d7a29
Dhanashree Mane <114648845+Dhanashreemane19@users.noreply.github.com>
2022-10-17 08:38:10 -0700

    Created encapsulation file in java

---
+++ encapsulationjava.java
@@ -0,0 +1,15 @@
+//A Java class which is a fully encapsulated class.  
+//It has a private data member and getter and setter methods.  
+package com.javatpoint;  
+public class Student{  
+//private data member  
+private String name;  
+//getter method for name  
+public String getName(){  
+return name;  
+}  
+//setter method for name  
+public void setName(String name){  
+this.name=name  
+}  
+}  
